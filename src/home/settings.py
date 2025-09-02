5d09858d2147ae3cc2468f1913a6c7ad69d226f9
Abhishek Anand <abhishek257101@gmail.com>
2025-09-02 23:31:18 +0530

    Using environment variable

--- src/home/settings.py
+++ src/home/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--*fy4+h4s*-!e53#l_n$j8nq+ymu$!wb@o&p)^=s#m#ld)n!vb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
 
 ALLOWED_HOSTS = [
     ".railway.app" # https://saas.prod.railway.app
