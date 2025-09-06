60e505bb5dca7b146dda68d68c0c2174d190967a
Abhishek Anand <abhishek257101@gmail.com>
2025-09-06 18:55:45 +0530

    add staging file

--- src/home/settings.py
+++ src/home/settings.py
@@ -142,6 +142,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+
+# source(s) for python manage.py collectstatics
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR
+]
+
+# output for python manage.py collectstatics
+# local cdn
+STATIC_ROOT = BASE_DIR.parent / "local-cdn"
+if DEBUG:
+    STATIC_ROOT = BASE_DIR / "local-cdn"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
